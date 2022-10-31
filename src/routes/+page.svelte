d0c2d250110e58c216c32b85f1e881913290b16e
sinYa Iwasaki <iwasaki@sinproject.net>
2022-10-31 16:45:15 +0900

    Add Web Speech API SpeechRecognition #12

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	// import { onMount } from "svelte";
 
-	let english_textarea: HTMLTextAreaElement
-	let japanese_textarea: HTMLTextAreaElement
+	let recognition_textarea: HTMLTextAreaElement;
+	let english_textarea: HTMLTextAreaElement;
+	let japanese_textarea: HTMLTextAreaElement;
 	// let language_selector: HTMLSelectElement
 
 	// function getVoices() {
@@ -26,17 +27,51 @@
 		utterance.rate = 1;
 		utterance.pitch = 1;
 		utterance.volume = 1;
-		
+
 		speechSynthesis.speak(utterance);
 	}
 
 	function speechInEnglish(): void {
-		speech(english_textarea.value, "en-US")
+		speech(english_textarea.value, 'en-US');
 	}
 
 	function speechInJapanese(): void {
-		speech(japanese_textarea.value, "ja-JP")
+		speech(japanese_textarea.value, 'ja-JP');
 	}
+
+	function recognition(lang: string): void {
+		if (!('webkitSpeechRecognition' in window)) {
+			recognition_textarea.value = 'Speech Recognition Not Available'
+			return
+		}
+
+		recognition_textarea.value = 'Recognition...'
+
+		const speech_recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+		const recognition = new speech_recognition();
+
+		recognition.lang = lang;
+		recognition.interimResults = false;
+		recognition.maxAlternatives = 1;
+
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		recognition.onresult = (event: any): void => {
+			const result = event.results[0][0].transcript;
+			recognition_textarea.value = result;
+		};
+
+		recognition.start();
+	}
+
+	function recognitionInEnglish(): void {
+		recognition('en-US');
+	}
+
+	function recognitionInJapanese(): void {
+		recognition('ja-JP');
+	}
+
+
 
 	// onMount(() => {
 	// 	getVoices()
@@ -44,21 +79,48 @@
 </script>
 
 <h1>Welcome to Talk</h1>
-<p>Visit <a href="https://github.com/sinProject-Inc/talk">https://github.com/sinProject-Inc/talk</a> to read the documentation</p>
+<p>
+	Visit <a href="https://github.com/sinProject-Inc/talk">https://github.com/sinProject-Inc/talk</a> to
+	read the documentation
+</p>
 
 <!-- <select bind:this={language_selector}></select> -->
 
-<textarea placeholder="Enter text to speech in English" size=60 value="Hello world!" bind:this={english_textarea} />
-<button on:click={speechInEnglish}>Speech in English</button>
+Speech Recognition
+<textarea
+	readonly
+	placeholder="Press the button and say something"
+	size="60"
+	bind:this={recognition_textarea}
+/>
+<button on:click={recognitionInEnglish}>English</button>
+<button on:click={recognitionInJapanese}>Japanese</button>
 
-<br><br>
+<br /><br />
 
-<textarea placeholder="Enter text to speech in Japanese" size=60 value="こんにちは、世界" bind:this={japanese_textarea} />
-<button on:click={speechInJapanese}>Speech in Japanese</button>
+English
+<textarea
+	placeholder="Enter text to speech in English"
+	size="60"
+	value="Hello world!"
+	bind:this={english_textarea}
+/>
+<button on:click={speechInEnglish}>Text-to-Speech</button>
+
+<br /><br />
+
+Japanese
+<textarea
+	placeholder="Enter text to speech in Japanese"
+	size="60"
+	value="こんにちは、世界"
+	bind:this={japanese_textarea}
+/>
+<button on:click={speechInJapanese}>Text-to-Speech</button>
 
 <style>
 	textarea {
-		width : 100%;
-		height: 200px;
+		width: 100%;
+		height: 100px;
 	}
 </style>
