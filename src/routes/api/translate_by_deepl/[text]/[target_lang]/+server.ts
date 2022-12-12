724a8d97ca3c0e79d35cd9c3cc13bc27cfdc343a
sinYa Iwasaki <iwasaki@sinproject.net>
2022-12-13 00:37:17 +0900

    Translate by Google #71

--- src/routes/api/translate_by_deepl/[text]/[target_lang]/+server.ts
+++ src/routes/api/translate_by_deepl/[text]/[target_lang]/+server.ts
@@ -3,7 +3,9 @@ import { DeepL } from '$lib/deepl'
 import { json, type RequestHandler } from '@sveltejs/kit'
 import type { TargetLanguageCode } from 'deepl-node'
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({ url, params }) => {
+	console.log(url.href)
+
 	const trimmed_text = params.text?.trim() ?? ''
 	// TODO: Translate to selected language #77
 	const target_lang = (params.target_lang?.trim() ?? 'en') as TargetLanguageCode
