11c650f070a0538434c5c36f06f38d26c81084a7
Ryan <ryan@websculptors.net>
2023-01-24 19:29:20 +0900

    Login screen design

--- src/routes/sign-in/+page.svelte
+++ src/routes/sign-in/+page.svelte
@@ -18,12 +18,45 @@
 	})
 </script>
 
-<h1>{$_('sign_in')} / {$_('sign_up')}</h1>
-
-<div class="flex_column">
-	<form method="POST" action="/pin-code?/sign_in&redirect_url={encoded_redirect_url}">
-		<input type="email" name="email" placeholder={$_('email')} required bind:this={pin_input_element} />
-		<button type="submit">{$_('sign_in')} / {$_('sign_up')}</button>
-		<input type="hidden" name="translated_pin_code" value="{$_('pin_code')}" />
-	</form>
+<div class="root_container flex_row align_items_center justify_content_center height_page">
+	<div class="center_container width_limit margin_bottom_128px">
+			<form class="flex_column card padding_16px gap_8px align_items_center" method="POST" action="/pin-code?/sign_in&redirect_url={encoded_redirect_url}">
+				<h1 class="title">{$_('sign_in')}</h1>
+				<input
+					class=""
+					type="email"
+					name="email"
+					placeholder={$_('email')}
+					required
+					bind:this={pin_input_element}
+				/>
+				<button class="submit_button" type="submit">{$_('sign_in')}</button>
+				<input type="hidden" name="translated_pin_code" value={$_('pin_code')} />
+			</form>
+	</div>
 </div>
+
+<style>
+	.width_limit {
+	margin-left: auto;
+	margin-right: auto;
+	width: 100%;
+	max-width: calc(500px + 2rem);
+}
+
+form > * {
+	width: 100%;
+	box-sizing: border-box;
+}
+
+.submit_button {
+	background-color: rgb(29, 155, 240);
+	color: white;
+	font-weight: 600;
+}
+
+.margin_bottom_128px {
+	margin-bottom: 128px;
+}
+
+</style>
