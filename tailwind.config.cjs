d65f449bb63fed71064556d83d0bba54eb9a4014
Ryan <ryan@websculptors.net>
2023-01-26 16:42:53 +0900

    feat: Set up Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+			},
+			fontFamily: {
+				sans: ['Segoe UI', 'Meiryo', ...defaultTheme.fontFamily.sans],
+			},
+			screens: {
+        xs: "410px",
+				"2xl": "1536px",
+			},
+    },
   },
   plugins: [],
 }
