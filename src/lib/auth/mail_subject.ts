4fc453ccd125ca8eb15b156f3c140f986828bfb3
sinYa Iwasaki <iwasaki@sinproject.net>
2023-01-30 23:19:45 +0900

    Refactor function arguments #171

---
+++ src/lib/auth/mail_subject.ts
@@ -0,0 +1,15 @@
+export class MailSubject {
+	private readonly _mail_subject: undefined
+	private readonly _subject: string
+
+	public constructor(subject: string) {
+		if (!subject) throw new Error('Mail subject is required')
+		if (subject.length > 100) throw new Error('Mail subject is too long')
+
+		this._subject = subject
+	}
+
+	public get subject(): string {
+		return this._subject
+	}
+}
