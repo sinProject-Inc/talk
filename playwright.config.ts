f7371d273f39e676e197fafa956aa8056e16637e
sinYa Iwasaki <iwasaki@sinproject.net>
2023-01-31 15:06:33 +0900

    Set up playwright #175 change timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,13 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 5 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 2000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
