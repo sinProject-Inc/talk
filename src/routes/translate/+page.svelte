4b6ddf2e4717f3046aebd703a8aef0d57652ba39
Ryan <ryan@websculptors.net>
2023-02-03 14:23:52 +0900

    feat: Make translation and main locale the same

--- src/routes/translate/+page.svelte
+++ src/routes/translate/+page.svelte
@@ -14,56 +14,64 @@
 
 	export let data: PageData
 
-	let top_locale_select_element: HTMLSelectElement
-	let bottom_locale_select_element: HTMLSelectElement
-
+	let from_locale_select_element: HTMLSelectElement
+	let to_locale_select_element: HTMLSelectElement
 	let from_language_text_element: HTMLTextAreaElement
 	let to_language_text_element: HTMLTextAreaElement
-
-	let top_locale_code = LocaleCode.english_united_states
-	let bottom_locale_code = LocaleCode.japanese_japan
-
-	let top_translate_box: TranslateBox
-	let bottom_translate_box: TranslateBox
-
 	let audio_element: HTMLAudioElement
 
-	let top_listening = false
-	let bottom_listening = false
+	let from_locale_code = LocaleCode.english_united_states
+	let to_locale_code = LocaleCode.japanese_japan
+
+	let from_translate_box: TranslateBox
+	let to_translate_box: TranslateBox
+
+	let from_listening = false
+	let to_listening = false
 
 	let text_history: Text[] = []
 	let selected_text: Text | undefined
 
-	$: listening = top_listening || bottom_listening
+	$: listening = from_listening || to_listening
 
 	function init_locale_select(): void {
 		const locales = JSON.parse(data.locales) as Locale[]
 
-		Html.append_language_select_options(top_locale_select_element, locales)
-		Html.append_language_select_options(bottom_locale_select_element, locales)
+		Html.append_language_select_options(from_locale_select_element, locales)
+		Html.append_language_select_options(to_locale_select_element, locales)
+	}
 
-		fetch_history()
+	async function select_default_locales(): Promise<void> {
+		const language_from = localStorage.getItem('from_locale')
+		from_locale_select_element.value = language_from ?? 'en-US'
+
+		const language_to = localStorage.getItem('to_locale')
+		to_locale_select_element.value = language_to ?? 'ja-JP'
+
+		on_change_locale_select(false)
 	}
 
 	function on_change_locale_select(store_locale = true): void {
 		if (!store_locale) {
-			const top_locale = localStorage.getItem('translate_top_locale')
-			const bottom_locale = localStorage.getItem('translate_bottom_locale')
+			const from_locale = localStorage.getItem('from_locale')
+			const to_locale = localStorage.getItem('to_locale')
 
-			if (top_locale) top_locale_select_element.value = top_locale
-			if (bottom_locale) bottom_locale_select_element.value = bottom_locale
+			if (from_locale) from_locale_select_element.value = from_locale
+			if (to_locale) to_locale_select_element.value = to_locale
 		}
 
-		const top_selected_value = top_locale_select_element.selectedOptions[0].value
-		const bottom_selected_value = bottom_locale_select_element.selectedOptions[0].value
+		const from_selected_value = from_locale_select_element.selectedOptions[0].value
+		const to_selected_value = to_locale_select_element.selectedOptions[0].value
 
-		top_locale_code = LocaleCode.create(top_selected_value)
-		bottom_locale_code = LocaleCode.create(bottom_selected_value)
+		from_locale_code = LocaleCode.create(from_selected_value)
+		to_locale_code = LocaleCode.create(to_selected_value)
 
 		if (store_locale) {
-			localStorage.setItem('translate_top_locale', top_locale_code.code)
-			localStorage.setItem('translate_bottom_locale', bottom_locale_code.code)
+			localStorage.setItem('from_locale', from_locale_code.code)
+			localStorage.setItem('to_locale', to_locale_code.code)
 		}
+
+		fetch_history()
 	}
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -84,29 +92,29 @@
 
 		if (event.detail.fetch_history) {
 			await fetch_history()
-			await top_translate_box.text_to_speech()
+			await to_translate_box.text_to_speech()
 		}
 	}
 
 	function switch_locales(): void {
-		const top_locale = top_locale_select_element.value
-		const bottom_locale = bottom_locale_select_element.value
+		const from_locale = from_locale_select_element.value
+		const to_locale = to_locale_select_element.value
 
-		top_locale_select_element.value = bottom_locale
-		bottom_locale_select_element.value = top_locale
+		from_locale_select_element.value = to_locale
+		to_locale_select_element.value = from_locale
 
-		const top_text = top_translate_box.get_text()
-		const bottom_text = bottom_translate_box.get_text()
+		const from_text = from_translate_box.get_text()
+		const to_text = to_translate_box.get_text()
 
-		top_translate_box.set_text(bottom_text)
-		bottom_translate_box.set_text(top_text)
+		from_translate_box.set_text(to_text)
+		to_translate_box.set_text(from_text)
 
 		on_change_locale_select()
 	}
 
 	async function fetch_history(): Promise<void> {
-		const speech_language_code = SpeechLanguageCode.create_from_locale_code(top_locale_code)
-		
+		const speech_language_code = SpeechLanguageCode.create_from_locale_code(from_locale_code)
+
 		text_history = await new TextsApi(speech_language_code, 10).fetch()
 	}
 
@@ -114,12 +122,13 @@
 		if (!browser) return
 
 		init_locale_select()
-		on_change_locale_select(false)
+
+		await select_default_locales()
 	})
 
 	async function on_click_text(text: Text): Promise<void> {
-		await top_translate_box.add_text(text.text)
-		await bottom_translate_box.show_translation(text)
+		await from_translate_box.add_text(text.text)
+		await to_translate_box.show_translation(text)
 	}
 </script>
 
@@ -134,7 +143,7 @@
 			class="outline-0 bg-transparent p-2 text-center hover:scale-110 transition-all duration-300 appearance-none text-ellipsis"
 			name="language_1"
 			id="language_1"
-			bind:this={top_locale_select_element}
+			bind:this={from_locale_select_element}
 			on:change={() => on_change_locale_select()}
 		/>
 		<IconButton on_click_handler={switch_locales}><SwapIcon /></IconButton>
@@ -142,36 +151,41 @@
 			class="outline-0 bg-transparent p-2 text-center hover:scale-110 transition-all duration-300 appearance-none text-ellipsis"
 			name="language_2"
 			id="language_2"
-			bind:this={bottom_locale_select_element}
+			bind:this={to_locale_select_element}
 			on:change={() => on_change_locale_select()}
 		/>
 	</div>
 	<div class="flex flex-col gap-4 h-full">
 		<TranslateBox
-			locale_select_element={top_locale_select_element}
+			locale_select_element={from_locale_select_element}
 			speech_text_element={from_language_text_element}
-			bind:this={top_translate_box}
+			bind:this={from_translate_box}
 			bind:audio_element
-			bind:locale_code={top_locale_code}
-			bind:listening={top_listening}
+			bind:locale_code={from_locale_code}
+			bind:listening={from_listening}
 			bind:either_listening={listening}
 			on:message={(event) => {
-				on_message(event, top_translate_box, bottom_translate_box)
+				on_message(event, from_translate_box, to_translate_box)
 			}}
 		/>
 		<TranslateBox
-			locale_select_element={bottom_locale_select_element}
+			locale_select_element={to_locale_select_element}
 			speech_text_element={to_language_text_element}
-			bind:this={bottom_translate_box}
+			bind:this={to_translate_box}
 			bind:audio_element
-			bind:locale_code={bottom_locale_code}
-			bind:listening={bottom_listening}
+			bind:locale_code={to_locale_code}
+			bind:listening={to_listening}
 			bind:either_listening={listening}
 			on:message={(event) => {
-				on_message(event, bottom_translate_box, top_translate_box)
+				on_message(event, to_translate_box, from_translate_box)
 			}}
 		/>
-		<div class="main-box history-box glass-panel h-[calc((100vh-190px)/3)] flex flex-col {text_history.length > 0 ? 'visible' : 'invisible' }">
+		<div
+			class="main-box history-box glass-panel h-[calc((100vh-190px)/3)] flex flex-col {text_history.length >
+			0
+				? 'visible'
+				: 'invisible'}"
+		>
 			<h2 class="title px-5 py-2">History</h2>
 			<div class="overflow-auto">
 				{#each text_history as text, i}
