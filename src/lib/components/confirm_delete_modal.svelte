bb26f4eafef821a268c1db2c87fd52137fea0c83
Ryan <ryan@websculptors.net>
2023-02-05 02:23:37 +0900

    locale: Delete confirmation

--- src/lib/components/confirm_delete_modal.svelte
+++ src/lib/components/confirm_delete_modal.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { createEventDispatcher, onMount } from 'svelte'
+  import { _ } from 'svelte-i18n'
 
 	const dispatch = createEventDispatcher()
 
@@ -40,11 +41,11 @@
 
 <div class="fixed top-0 left-0 w-full h-full justify-center items-center flex pointer-events-none">
   <div class="py-3 glass-panel pointer-events-auto px-5 text-center">
-    <div class="font-bold text-lg mb-3">Confirm Delete</div>
-    <div class="mb-4">Are you sure you want to delete this?</div>
+    <div class="font-bold text-lg mb-3">{$_('confirm_delete')}</div>
+    <div class="mb-4">{$_('are_you_sure') }</div>
     <div class="flex justify-center gap-4">
-      <button class="glass-button bg-white/5 hover:bg-white/10 delete-cancel" on:click={close}>Cancel</button>
-      <button class="glass-button text-red-400 delete-confirm" on:click={confirm_delete}>Delete</button>
+      <button class="glass-button bg-white/5 hover:bg-white/10 delete-cancel" on:click={close}>{$_('cancel')}</button>
+      <button class="glass-button text-red-400 delete-confirm" on:click={confirm_delete}>{$_('delete')}</button>
     </div>
   </div>
 </div>
