139fca21c504b7717da9472598f98d87250a82a2
Ryan <ryan@websculptors.net>
2023-02-07 17:42:02 +0900

    Remove unused variable 'languages' #256

--- src/routes/translate/+page.server.ts
+++ src/routes/translate/+page.server.ts
@@ -1,9 +1,7 @@
-import { LanguagesApi } from '$lib/language/languages_api'
 import { LocalesApi } from '$lib/language/locales_api'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ url }) => {
-	const languages = await new LanguagesApi(url.origin).fetch()
 	const locales = await new LocalesApi(url.origin).fetch()
 
 	return { 
