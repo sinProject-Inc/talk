d25b1029690df35f1b16a045c1da2806e2499c59
Ryan <ryan@websculptors.net>
2023-02-09 13:12:57 +0900

    translate: Limit to 250 characters #258 Use valid text in text error

--- src/lib/general/text_error.ts
+++ src/lib/general/text_error.ts
@@ -1,9 +1,13 @@
+import { ValidText } from "$lib/text/valid_text"
+
 export class TextError {
   private readonly _text_error: undefined
   private readonly _message_id: string
 
   public constructor(message_id: string) {
-    this._message_id = message_id
+		const valid_text = new ValidText(message_id)
+
+    this._message_id = valid_text.text
   }
 
   public get message_id(): string {
