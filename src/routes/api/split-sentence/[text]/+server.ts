fe9fdfb9c15d2a2e5e007c27b59be650f7356041
sinYa Iwasaki <iwasaki@sinproject.net>
2023-02-19 15:23:08 +0900

    Use value objects #321 Rename StringUIlt to SentenceService

--- src/routes/api/split-sentence/[text]/+server.ts
+++ src/routes/api/split-sentence/[text]/+server.ts
@@ -1,5 +1,5 @@
 
-import { SentenceService } from '$lib/general/string_util'
+import { SentenceService } from '$lib/general/sentence_service'
 import { json, type RequestHandler } from '@sveltejs/kit'
 
 export const GET: RequestHandler = async ({ params }) => {
