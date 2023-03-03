9e26772c634068592edacebf10111fe6bb8f8e17
sinYa Iwasaki <iwasaki@sinproject.net>
2023-03-03 17:37:46 +0900

    Implement WebSockets #360 Adjust UI / Support sending name

--- socket-handler.js
+++ socket-handler.js
@@ -4,11 +4,11 @@ export default function inject_socket_io(server) {
 	const io = new Server(server)
 
 	io.on('connection', (socket) => {
-		socket.emit('message', 'Hello from the server')
+		socket.emit('message', { name: 'SERVER', message: 'Hello from the server' })
 
-		socket.on('message', (message) => {
-			io.emit('message', message)
-			console.info('message:', message)
+		socket.on('message', (message_set) => {
+			io.emit('message', message_set)
+			console.info('message:', message_set.name, message_set.message)
 		})
 	})
 }
--- src/routes/web-socket/+page.svelte
+++ src/routes/web-socket/+page.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
+	import FillIcon from '$lib/components/icons/fill_icon.svelte'
+	import IconButton from '$lib/components/icon_button.svelte'
+	import Navbar from '$lib/components/navbar.svelte'
+	import { EventKey } from '$lib/view/event_key'
 	import { io } from 'socket.io-client'
+	import { onMount } from 'svelte'
+	import { _ } from 'svelte-i18n'
 
+	type MessageSet = {
+		name: string
+		message: string
+	}
+
+	let name_element: HTMLInputElement
+	let message_element: HTMLInputElement
+
+	let name = ''
 	let message = ''
-	let messages: string[] = []
+	let message_data: MessageSet[] = []
 
 	const socket = io()
 
@@ -14,21 +29,76 @@
 		console.info('socket.io disconnected')
 	})
 
-	socket.on('message', (data: string) => {
-		console.info(`socket.io message: ${data}`)
-		
-		messages = [data, ...messages]
+	socket.on('message', (received_data: MessageSet) => {
+		console.info(`socket.io message: ${received_data}`)
+
+		message_data = [received_data, ...message_data]
+
+		if (received_data.name !== name) return
+		if (received_data.message !== message) return
+
+		// TODO: 厳密同一人物チェックが必要
+		message = ''
 	})
 
 	async function send(): Promise<void> {
 		console.info(`socket.io send: ${message}`)
-		socket.emit('message', message)
+		socket.emit('message', { name, message })
 	}
+
+	function on_keydown_name(event: KeyboardEvent): void {
+		const event_key = new EventKey(event)
+
+		if (!event_key.is_enter) return
+		if (!name) return
+
+		event.preventDefault()
+		message_element.focus()
+	}
+
+	function on_keydown_message(event: KeyboardEvent): void {
+		const event_key = new EventKey(event)
+
+		if (!event_key.is_enter) return
+
+		event.preventDefault()
+		send()
+	}
+
+	onMount(() => {
+		name_element.focus()
+	})
 </script>
 
-<input type="text" bind:value={message} />
-<button on:click={send}>Send</button>
+<svelte:head>
+	<title>Talk - Chat</title>
+</svelte:head>
 
-{#each messages as msg}
-	<p>{msg}</p>
-{/each}
+<Navbar />
+
+<div class="center-container w-screen h-[calc(100vh-69px)]">
+	<div class="p-4 glass-panel my-4 flex flex-col gap-4 flex-1 overflow-y-scroll">
+		<input
+			type="text"
+			bind:this={name_element}
+			bind:value={name}
+			placeholder="Name"
+			class="w-60"
+			on:keydown={on_keydown_name}
+		/>
+		<div class="flex gap-2 items-center">
+			<input
+				type="text"
+				class="flex-1"
+				placeholder={$_('enter_new_text')}
+				bind:this={message_element}
+				bind:value={message}
+				on:keydown={on_keydown_message}
+			/>
+			<IconButton on_click_handler={send}><FillIcon /></IconButton>
+		</div>
+		{#each message_data as message_set}
+			<p>{message_set.name}: {message_set.message}</p>
+		{/each}
+	</div>
+</div>
