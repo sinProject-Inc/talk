0926eb7f5de967b002fa67a3dc9e122bcd2526c0
sinYa Iwasaki <iwasaki@sinproject.net>
2023-03-21 23:30:06 +0900

    Output log file #429 output log by winston

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,19 @@
+import { logger } from '$lib/app/logger'
 import { LocalesApi } from '$lib/locale/locales_api'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ url, fetch }) => {
+	logger.info(`GET ${url}`)
+
+	logger.debug('[main] debug')
+	logger.verbose('[main] verbose')
+	logger.info('[main] Load')
+	logger.warn('[main] Warn test')
+	logger.error('[main] Error', new Error('エラーやでー'))
+
 	const locales = await new LocalesApi(fetch).fetch()
 
-	return { 
-		locales: JSON.stringify(locales)
+	return {
+		locales: JSON.stringify(locales),
 	}
 }
