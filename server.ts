0e937b974538b3b1b9526df302a74809598841b6
sinYa Iwasaki <iwasaki@sinproject.net>
2023-03-22 12:34:21 +0900

    Catch uncaughtException #434

--- server.ts
+++ server.ts
@@ -2,7 +2,26 @@ import express from 'express'
 import http from 'http'
 import { handler } from './build/handler.js' // <- Import SvelteKit handlers
 import inject_socket_io from './socket-handler' // The SocketIO stuff (see next step)
-import { logger } from './src/lib/app/logger' // The SocketIO stuff (see next step)
+import { logger } from './src/lib/app/logger'
+
+process.on('unhandledRejection', (reason, promise) => {
+	logger.error('[process] Unhandled Rejection:', promise, 'reason:', reason)
+	console.log('Unhandled Rejection at:', promise, 'reason:', reason)
+})
+
+process.on('uncaughtException', (error) => {
+	logger.error('[process] Uncaught Exception:', error)
+	console.log('Uncaught Exception at:', error)
+	process.exit(1)
+})
+
+function shutdown_gracefully(): void {
+	logger.info('[process] Shutting down gracefully')
+	process.exit(1)
+}
+
+process.on('SIGINT', shutdown_gracefully)
+process.on('SIGTERM', shutdown_gracefully)
 
 const app = express()
 const server = http.createServer(app)
--- src/lib/translation/deepl_translator.ts
+++ src/lib/translation/deepl_translator.ts
@@ -5,7 +5,10 @@ import * as deepl from 'deepl-node'
 import { TranslationText } from './translation_text'
 
 export class DeeplTranslator {
-	public constructor(private readonly _translation_text: TranslationText, private readonly _source_language_code: SourceLanguageCode | null = null) {}
+	public constructor(
+		private readonly _translation_text: TranslationText,
+		private readonly _source_language_code: SourceLanguageCode | null = null
+	) {}
 
 	public async translate(target_language_code: TargetLanguageCode): Promise<TranslationText> {
 		const translator = new deepl.Translator(DEEPL_AUTH_KEY)
@@ -17,7 +20,7 @@ export class DeeplTranslator {
 		)
 		const translated_text = new TranslationText(result.text)
 
-		logger.info(`[DeepL] Translated from ${this._source_language_code} to ${target_language_code})
+		logger.info(`[DeepL] Translated from ${this._source_language_code} to ${target_language_code}`)
 
 		return translated_text
 	}
