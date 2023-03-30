1d4b0562acb4d1454fda09792458e618bb3778af
sinYa Iwasaki <iwasaki@sinproject.net>
2023-03-30 18:25:15 +0900

    Run the local dev server before starting the playwright tests #468

--- playwright.config.ts
+++ playwright.config.ts
@@ -101,10 +101,12 @@ const config: PlaywrightTestConfig = {
 	// outputDir: 'test-results/',
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   port: 3000,
-	// },
+	webServer: {
+		command: 'npm run dev -- --port 5273',
+		port: 5273,
+		// command: 'npm run start',
+		// port: 3000,
+	},
 }
 
 export default config
