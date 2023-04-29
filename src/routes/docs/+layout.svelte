3a368c3ae898e9cf23dd8129def0b581824de81e
milkyskies <ryan@websculptors.net>
2023-04-29 14:50:52 +0900

    Create doc pages #580

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -6,22 +6,13 @@
 	$: sections = data?.sections ?? []
 </script>
 
-<div class="max-w-7xl min-h-screen mx-auto">
-	<div class="w-72 ps-8 pe-4 pb-8 h-screen fixed overflow-y-auto">
-		<SideBar {sections} />
-	</div>
-	<div class="ps-80 py-8">
-		<slot />
+<div class="doc-base">
+	<div class="max-w-7xl min-h-screen mx-auto">
+		<div class="w-72 ps-8 pe-4 pb-8 h-screen fixed overflow-y-auto">
+			<SideBar {sections} />
+		</div>
+		<div class="ps-80 py-8">
+			<slot />
+		</div>
 	</div>
 </div>
-
-<!-- <style>
-	.doc-base {
-		background: rgba(0, 0, 0, 0.25);
-		box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-		/* backdrop-filter: blur(4px); */
-		-webkit-backdrop-filter: blur(4px);
-		border-radius: 10px;
-		border: 1px solid rgba(255, 255, 255, 0.18);
-	}
-</style> -->
