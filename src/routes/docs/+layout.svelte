e9de7bb5a091e503386fa2e7534ed5f87890b548
sakata0411 <taaa160@yahoo.co.jp>
2023-05-04 19:17:15 +0900

    Docs: Add page transitions #662

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import Navbar from '$lib/components/navbar.svelte'
 	import SideBar from './side-bar.svelte'
+	import { page } from '$app/stores'
+	import { fly } from 'svelte/transition'
 
 	export let data
 
+	$: pathname = $page.url.pathname
 	$: sections = data?.sections ?? []
 </script>
 
@@ -17,7 +20,11 @@
 			<SideBar {sections} />
 		</div>
 		<div class="ps-12 pe-8 md:ps-80 py-8 xl:pe-80">
-			<slot />
+			{#key pathname}
+				<div in:fly={{ y: 5, duration: 600 }}>
+					<slot />
+				</div>
+			{/key}
 		</div>
 	</div>
 </div>
