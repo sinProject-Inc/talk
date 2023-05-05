a1011c9823d6db9bc70a105cf4cc40a9a595a504
milkyskies <ryan@websculptors.net>
2023-05-05 17:06:02 +0900

    Docs: search #606 Refactor

--- src/lib/docs/search_result_context.ts
+++ src/lib/docs/search_result_context.ts
@@ -3,7 +3,7 @@ import type Fuse from 'fuse.js'
 export interface SplitContextPortion {
 	text: string
 	is_match: boolean
-	string_index: number
+	first_character_index: number
 }
 
 interface IndexRange {
@@ -36,14 +36,14 @@ export class SearchResultContext {
 				split_context.push({
 					text: text.slice(last_index, match.index),
 					is_match: false,
-					string_index: last_index,
+					first_character_index: last_index,
 				})
 			}
 
 			split_context.push({
 				text: match[0],
 				is_match: true,
-				string_index: match.index,
+				first_character_index: match.index,
 			})
 
 			last_index = match.index + match[0].length
@@ -53,7 +53,7 @@ export class SearchResultContext {
 			split_context.push({
 				text: text.slice(last_index),
 				is_match: false,
-				string_index: last_index,
+				first_character_index: last_index,
 			})
 		}
 
@@ -86,20 +86,20 @@ export class SearchResultContext {
 		const starting_portion_shortened = {
 			...starting_portion,
 			text: starting_portion.text.slice(
-				index_range.start - starting_portion.string_index,
+				index_range.start - starting_portion.first_character_index,
 				starting_portion.text.length
 			),
 		}
 
 		const ending_portion_shortened = {
 			...ending_portion,
-			text: ending_portion.text.slice(0, index_range.end - ending_portion.string_index),
+			text: ending_portion.text.slice(0, index_range.end - ending_portion.first_character_index),
 		}
 
 		const middle_portions = split_context.filter((portion) => {
 			return (
-				portion.string_index > starting_portion.string_index &&
-				portion.string_index < ending_portion.string_index
+				portion.first_character_index > starting_portion.first_character_index &&
+				portion.first_character_index < ending_portion.first_character_index
 			)
 		})
 
@@ -121,8 +121,8 @@ export class SearchResultContext {
 		const remaining_length = max_length - match_length
 
 		if (remaining_length <= 0) {
-			const start = matching_portion.string_index
-			const end = matching_portion.string_index + match_length
+			const start = matching_portion.first_character_index
+			const end = matching_portion.first_character_index + match_length
 
 			return { start, end }
 		}
@@ -130,8 +130,8 @@ export class SearchResultContext {
 		const left_length = Math.floor(remaining_length / 2)
 		const right_length = remaining_length - left_length
 
-		let start = matching_portion.string_index - left_length
-		let end = matching_portion.string_index + match_length + right_length
+		let start = matching_portion.first_character_index - left_length
+		let end = matching_portion.first_character_index + match_length + right_length
 
 		if (start < 0) {
 			end += Math.abs(start)
@@ -151,8 +151,8 @@ export class SearchResultContext {
 	}
 
 	private _is_index_in_portion(portion: SplitContextPortion, index: number): boolean {
-		const portion_start_index = portion.string_index
-		const portion_end_index = portion.string_index + portion.text.length
+		const portion_start_index = portion.first_character_index
+		const portion_end_index = portion.first_character_index + portion.text.length
 
 		const is_in_portion = portion_start_index <= index && index <= portion_end_index
 
