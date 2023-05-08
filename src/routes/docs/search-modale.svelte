60951ed8ea180be0179a5162a4da6ee6a122e56f
sakata0411 <taaa160@yahoo.co.jp>
2023-05-08 16:13:11 +0900

    Docs: Add ESC button to search result window #692

--- src/routes/docs/search-modale.svelte
+++ src/routes/docs/search-modale.svelte
@@ -189,7 +189,7 @@
 		class="rounded-xl glass-panel bg-slate-900/90 backdrop-blur-md pointer-events-auto text-center mx-auto max-w-screen-md w-full h-fit max-h-[calc(75vh)] flex flex-col"
 	>
 		<form class="px-4 py-3" on:submit|preventDefault={get_search_results} autocomplete="off">
-			<div class="flex">
+			<div class="flex items-center">
 				<label class="w-7" for="search"><SearchIcon /></label>
 				<input
 					class="w-full pl-4 text-xl bg-inherit"
@@ -200,6 +200,13 @@
 					placeholder="Search documentation"
 					id="search"
 				/>
+				<div
+					class="flex justify-center items-center bg-gray-600 rounded-md px-2 h-8 cursor-default text-sm"
+					on:click={close}
+					on:keydown
+				>
+					ESC
+				</div>
 			</div>
 		</form>
 		<div class="w-full h-[1px] bg-white/20" />
