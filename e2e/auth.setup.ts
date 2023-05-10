baa87b8330282cf4adf290b927a368e2187b4852
sinYa Iwasaki <iwasaki@sinproject.net>
2023-05-10 18:54:43 +0900

    Run Playwright's tests in CI #741

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -1,18 +1,44 @@
 import { expect, test as setup } from '@playwright/test'
-import { sleep } from '../src/lib/general/system.js'
-import { get_pin_code_from_mail } from './lib/get_pin_code_from_mail.js'
+import { PrismaClient } from '@prisma/client'
+// import { promises as fs } from 'fs'
+// import { sleep } from '../src/lib/general/system.js'
+// import { get_pin_code_from_mail } from './lib/get_pin_code_from_mail.js'
 import { auth_file_path } from './lib/setup.js'
-import { promises as fs } from 'fs'
+
+// async function find_auth_file(): Promise<boolean> {
+// 	try {
+// 		await fs.access(auth_file_path, fs.constants.F_OK)
+// 		return true
+// 	} catch {
+// 		return false
+// 	}
+// }
+
+// async function get_pin_code_from_mail(): Promise<string> {
+// 	await sleep(process.env.CI ? 5000 : 1000)
+// 	const pin_code = await get_pin_code_from_mail()
+// 	expect(pin_code).toMatch(/^\d{6}$/)
+
+// 	return pin_code
+// }
+
+async function get_pin_code_from_database(gmail_user: string): Promise<string> {
+	const prisma_client = new PrismaClient()
+
+	const auth_pin = await prisma_client.authPin.findFirst({
+		where: { user: { email: gmail_user } },
+		orderBy: { updated_at: 'desc' },
+	})
+
+	if (!auth_pin) throw Error('auth_pin is null')
+
+	return auth_pin.pin_code
+}
 
 setup('sign in', async ({ page }) => {
 	setup.setTimeout(20 * 1000)
 
-	try {
-		await fs.access(auth_file_path, fs.constants.F_OK)
-		return
-	} catch {
-		// DO NEXT
-	}
+	// if (await find_auth_file()) return
 
 	await page.goto('/sign-in')
 
@@ -26,11 +52,7 @@ setup('sign in', async ({ page }) => {
 
 	await expect(page).toHaveTitle('Talk - PIN code')
 
-	await sleep(process.env.CI ? 5000 : 1000)
-
-	const pin_code = await get_pin_code_from_mail()
-
-	expect(pin_code).toMatch(/^\d{6}$/)
+	const pin_code = await get_pin_code_from_database(gmail_user)
 
 	await page.getByPlaceholder('PIN code').fill(pin_code)
 	await page.getByRole('button', { name: 'Submit' }).click()
