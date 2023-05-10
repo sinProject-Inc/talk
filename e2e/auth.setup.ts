170f7db23ce19fda7825d33b1641cf4b648f7d95
sinYa Iwasaki <iwasaki@sinproject.net>
2023-05-10 19:08:56 +0900

    Run Playwright's tests in CI #741

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -49,6 +49,7 @@ setup('sign in', async ({ page }) => {
 	await page.getByPlaceholder('Enter email').fill(gmail_user)
 	await page.getByRole('button', { name: 'Continue' }).click()
 	await page.waitForURL(/\/pin-code/)
+	await page.waitForLoadState('networkidle')
 
 	await expect(page).toHaveTitle('Talk - PIN code')
 
