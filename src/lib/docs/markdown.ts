52a5db592251311206b562194d02ec35f64d01e9
sinYa Iwasaki <iwasaki@sinproject.net>
2023-05-20 11:55:01 +0900

    Docs: Fix code title link area #864

--- src/lib/docs/markdown.ts
+++ src/lib/docs/markdown.ts
@@ -124,8 +124,8 @@ export class Markdown {
 
 			if (filename) {
 				filename_tag = `
-					<div>
-						<a class="code-title font-semibold text-slate-400 hover:text-slate-300" href="${github_url}${filename}" target="blank">
+					<div class="flex gap-2 justify-between">
+						<a class="code-title flex gap-1.5 items-center font-semibold text-slate-400 hover:text-slate-300" href="${github_url}${filename}" target="blank">
 							<div style="width:20px">
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
@@ -149,6 +149,27 @@ export class Markdown {
 							</div>
 							${filename}
 						</a>
+						<div style="width:20px; display: none">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="icon icon-tabler icon-tabler-copy"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								stroke-width="2"
+								stroke="currentColor"
+								fill="none"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							>
+								<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+								<path
+									d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"
+									fill="none"
+								/>
+								<path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" fill="none" />
+							</svg>
+						</div>
 					</div>
 				`
 			}
