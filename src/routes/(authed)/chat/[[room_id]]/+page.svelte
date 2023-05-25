293f8ae66ed85496c15b8f57c1af3ab090710b3d
sakata0411 <taaa160@yahoo.co.jp>
2023-05-25 15:05:16 +0900

    Decrease glass transparency #594

--- src/routes/(authed)/chat/[[room_id]]/+page.svelte
+++ src/routes/(authed)/chat/[[room_id]]/+page.svelte
@@ -570,7 +570,7 @@
 			</div>
 			{#if !joined}
 				<button
-					class="glass-button glass-panel flex h-[40px] items-center"
+					class="glass-button glass-panel flex h-[40px] items-center bg-black/40 hover:bg-black/20"
 					on:click={new_room}
 					data-testid="new-room-button"
 				>
