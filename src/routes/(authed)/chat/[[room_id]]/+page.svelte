2166ddde69372eb2d305e7052c35aa87732b2667
sakata0411 <taaa160@yahoo.co.jp>
2023-06-04 16:14:21 +0900

    Fix chat margins #1021

--- src/routes/(authed)/chat/[[room_id]]/+page.svelte
+++ src/routes/(authed)/chat/[[room_id]]/+page.svelte
@@ -562,7 +562,7 @@
 <div class="flex h-screen min-h-screen flex-col">
 	<Navbar />
 
-	<div class="center-container mt-4 flex w-screen flex-1 flex-col gap-3 overflow-y-auto">
+	<div class="center-container flex w-screen flex-1 flex-col gap-3 overflow-y-auto p-3">
 		<div class="flex items-center justify-between gap-3">
 			<div class="glass-panel flex h-[40px] w-full items-center gap-3 p-3">
 				<div class="ml-1 font-bold text-white/80">{$_('room')}:</div>
