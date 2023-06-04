dab8622a61ac5d2e27419434229375ca4f01d89f
milkyskies <ryan@websculptors.net>
2023-06-05 01:35:35 +0900

    Profile: DB Integration #943

--- src/lib/auth/user_repository.ts
+++ src/lib/auth/user_repository.ts
@@ -1,7 +1,8 @@
-import type { Theme, User } from '@prisma/client'
+import type { Theme, User, AvatarExtension } from '@prisma/client'
 import type { Email } from './email'
 
 export interface UserRepository {
 	find_unique(email: Email, can_register?: boolean): Promise<User | undefined>
 	update_theme(user: User, theme: string): Promise<Theme | undefined>
+	update_avatar_extension(user: User, extension: AvatarExtension): Promise<string | undefined>
 }
