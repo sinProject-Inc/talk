4cac2fb70e6c5a20475ea5802c138de9eb8639f6
sakata0411 <taaa160@yahoo.co.jp>
2023-06-05 19:37:46 +0900

    Header: Change the line width of the active bottom line to 1px #1051

--- src/lib/components/tab.svelte
+++ src/lib/components/tab.svelte
@@ -8,6 +8,6 @@
 
 <style lang="postcss">
 	.active {
-		@apply border-b-2 border-secondary pt-[2px] dark:border-secondary-dark;
+		@apply border-b border-secondary pt-[2px] dark:border-secondary-dark;
 	}
 </style>
