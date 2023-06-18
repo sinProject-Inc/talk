1db476c49e114578f30af7b841442d8add7c7995
sinYa Iwasaki <iwasaki@sinproject.net>
2023-06-18 18:32:41 +0900

    Intro page #1078

--- e2e/main.spec.ts
+++ e2e/main.spec.ts
@@ -1,59 +1,9 @@
-import { test, expect } from '@playwright/test'
-import { auth_file_path } from './lib/setup.js'
+import { expect, test } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
 	await page.goto('./', { waitUntil: 'networkidle' })
 })
 
 test('before sign in', async ({ page }) => {
-	await expect(page).toHaveTitle('Sign in - Talk')
-})
-
-test.describe('after sign in', () => {
-	test.use({ storageState: auth_file_path })
-
-	test('has title', async ({ page }) => {
-		await expect(page).toHaveTitle('Talk')
-	})
-
-	// test('sign in button', async ({ page }) => {
-	// 	await page.getByRole('button', { name: 'Sign in' }).click()
-	// 	await expect(page).toHaveURL(/sign-in/)
-	// })
-
-	test('from locale combo box', async ({ page }) => {
-		await expect(page.getByRole('combobox').nth(0)).toHaveValue('en-US')
-	})
-
-	test('to locale combo box', async ({ page }) => {
-		await expect(page.getByRole('combobox').nth(1)).toHaveValue('ja-JP')
-	})
-
-	test.describe('after sign in', () => {
-		test.use({ storageState: auth_file_path })
-
-		test('changing locale, and then moving pages keeps saved locale', async ({ page }) => {
-			await page.getByRole('combobox').first().selectOption('yue-HK')
-			await page.getByRole('combobox').last().selectOption('km-KH')
-
-			page.waitForTimeout(500)
-
-			await page.goto('./translate', { waitUntil: 'networkidle' })
-
-			await expect(page.getByRole('combobox').first()).toHaveValue('km-KH')
-			await expect(page.getByRole('combobox').last()).toHaveValue('yue-HK')
-		})
-	})
-
-	// TODO: GitHub Actions で動作させるにはデータが必要
-	// test('If there is text on translate, there is text on main', async ({ page }) => {
-	// 	await page.waitForSelector('.text')
-	// 	const main_text_count = await page.locator('.text').count()
-
-	// 	await page.goto('/translate')
-	// 	await page.waitForSelector('.text')
-	// 	const translate_page_count = await page.locator('.text').count()
-
-	// 	await expect(main_text_count).toBeGreaterThanOrEqual(translate_page_count)
-	// })
+	await expect(page).toHaveTitle('Talk')
 })
