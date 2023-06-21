5428bcfe40520424c9b697677264284874c32ccc
sinYa Iwasaki <iwasaki@sinproject.net>
2023-06-21 20:22:03 +0900

    Fix locale not updating on refresh on main, chat, docs, and profile #1174 fix tests

--- e2e/learn.spec.ts
+++ e2e/learn.spec.ts
@@ -46,13 +46,15 @@ test.describe('after sign in', () => {
 
 		test('change locale', async ({ page }) => {
 			const locale_combobox = page.getByTestId('from-locale-select')
-
 			await locale_combobox.selectOption('ja-JP')
 			await expect(page.locator('.nav-item-text').first()).toHaveText('学ぶ')
 
 			await page.goto('/', { waitUntil: 'networkidle' })
-
 			await expect(page.locator('.nav-item-text').first()).toHaveText('学ぶ')
+
+			await page.goto('./learn', { waitUntil: 'networkidle' })
+			const locale_combobox2 = page.getByTestId('from-locale-select')
+			await locale_combobox2.selectOption('en-US')
 		})
 	})
 
