45f248e8e1434474ed0a4b82020d9d8b29b7ee7d
sinYa Iwasaki <iwasaki@sinproject.net>
2023-09-13 13:54:30 +0900

    Fix the security hotspots pointed out by SonarCloud #1382

--- src/lib/auth/email.ts
+++ src/lib/auth/email.ts
@@ -4,8 +4,7 @@ export class Email {
 	public constructor(address: string | undefined) {
 		if (!address) throw new Error('Email address is required')
 
-		const pattern =
-			/^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/
+		const pattern = /^[a-zA-Z0-9._%+-]{1,64}@[a-zA-Z0-9.-]{1,255}\.[a-zA-Z]{2,}$/
 
 		if (!pattern.test(address)) {
 			throw new Error('Invalid email address')
