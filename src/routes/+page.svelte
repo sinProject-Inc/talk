55380976a7d5e8358ff161a8c9be4c4aa95dab8d
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-09-15 07:22:11 +0900

    Strengthen eslint rules #1377 Modify a script tag in src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { App } from '$lib/app/app'
 	import Navbar from '$lib/components/navbar.svelte'
 	import SocialMetaTags from '$lib/components/social_meta_tags.svelte'
