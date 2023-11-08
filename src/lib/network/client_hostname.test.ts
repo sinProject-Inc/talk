f736bfee0cb1b08d350804cb34529c407ab9168d
REI-MORI <morirei750@gmail.com>
2023-11-08 11:30:11 +0900

    Set tsconfig to strictest #1445 Fix test case

--- src/lib/network/client_hostname.test.ts
+++ src/lib/network/client_hostname.test.ts
@@ -1,18 +1,38 @@
 import { ClientHostName } from './client_hostname'
-import { expect, test, describe } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
+
+vi.mock('dns', async () => {
+	const actual_dns = (await vi.importActual('dns')) as typeof import('dns')
+
+	return {
+		...actual_dns,
+		promises: {
+			reverse: vi.fn((ip) =>
+				ip === '8.8.8.8'
+					? Promise.resolve(['dns.google'])
+					: Promise.reject(new Error('getHostByAddr EINVAL'))
+			),
+		},
+	}
+})
 
 describe('ClientHostName', () => {
 	test('should return the hostname for a valid IP address', async () => {
 		const client_host_name = new ClientHostName('8.8.8.8')
 		const hostname = await client_host_name.reverse()
-
 		expect(hostname).toBe('dns.google')
 	})
 
 	test('should return an empty string for an invalid IP address', async () => {
 		const client_host_name = new ClientHostName('999.999.999.999')
-		const hostname = await client_host_name.reverse()
 
+		let hostname
+
+		try {
+			hostname = await client_host_name.reverse()
+		} catch (error) {
+			hostname = ''
+		}
 		expect(hostname).toBe('')
 	})
 })
