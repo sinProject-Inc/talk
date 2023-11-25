27c0dac5885add87443fc1a9afed0331f2d7ab0f
REI-MORI <morirei750@gmail.com>
2023-11-25 10:24:52 +0900

    Configure Playwright #1451

--- tests/test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('before sign in', async ({ page }) => {
-	await page.goto('/', { waitUntil: 'networkidle' })
-	await expect(page).toHaveTitle('Talk')
-})
